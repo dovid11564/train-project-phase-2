157fd5dec28d6e4db361a53fdcab0b082fa837e3
Dovid Edelkopf <dovidzedelkopf@gmail.com>
2022-11-14 15:28:49 -0500

    innitial leaflet map! ^.^

--- src/Map.js
+++ src/Map.js
@@ -1,6 +1,19 @@
-function Map () {
-    return(
-        <p>map</p>
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
+import './App.css';
+
+function Map() {
+    return (
+        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[51.505, -0.09]}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+        </MapContainer>
     )
 }
 
--- src/Page.js
+++ src/Page.js
@@ -1,14 +1,26 @@
+import { useEffect, useState } from "react"
 import NavBar from "./NavBar"
 import SideBar from "./SideBar"
 import Map from "./Map"
 
 function Page () {
+
+    //creating state to hold subway station data
+    const [stations, setStations] = useState([])
+
+    //useEffect fetches here
+
+    useEffect(() => {
+        fetch("https://data.cityofnewyork.us/resource/kk4q-3rt2.json")
+        .then(response => response.json())
+        .then(data => setStations(data))
+    })
     return(
         <>
         <p>page</p>
         <NavBar />
         <SideBar />
-        <Map />
+        <Map stations={stations} />
         </>
     )
 }
